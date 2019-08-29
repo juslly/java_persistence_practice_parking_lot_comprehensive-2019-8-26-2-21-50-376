0e2f581860d945ecccc6c4923caf2e4ffe3c0cfa
zhangjing <juslly@163.com>
2019-08-29 23:58:06 +0800

    push MockTest

--- src/test/java/tws/repository/ParkingLotControllerTest.java
+++ src/test/java/tws/repository/ParkingLotControllerTest.java
@@ -1,5 +1,9 @@
 package tws.repository;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,23 +13,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import com.fasterxml.jackson.core.PrettyPrinter;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*; 
-
-import tws.controller.EmployeeController;
+import tws.controller.ParkingLotController;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(EmployeeController.class) // 需要测试的 Controller
-public class EmployeeControllerTest {
+@WebMvcTest(ParkingLotController.class) // 需要测试的 Controller
+public class ParkingLotControllerTest {
 	
 	@Autowired
     private MockMvc mockMvc;
 	
 	@Test
-    public void should_return_all_employees() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(get("/employees")
+    public void should_return_all_parkingLots() throws Exception {
+        MvcResult mvcResult = mockMvc.perform(get("/parkingLots")
         		.andDo(print())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
@@ -36,16 +35,16 @@ public class EmployeeControllerTest {
 	
 	@Test
     public void insert_test() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(post("/employees") 
+        MvcResult mvcResult = mockMvc.perform(post("/parkingLots") 
                 .accept(MediaType.APPLICATION_JSON) 
-                .param(1, "tom",23)
+                .param(1,30,20)
                 .contentType(MediaType.APPLICATION_JSON)) 
                 .andExpect(status().isOk())
                 .andDo(print()) 
                 .andReturn(); // 获取返回值MvcResult
         System.out.println(mvcResult.getResponse().getContentAsString());
 
-        should_return_all_employees();
+        should_return_all_parkingLots();
     }
 
 }
